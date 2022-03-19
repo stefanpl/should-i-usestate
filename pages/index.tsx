c5b3e482d6763bef17c6da62ce4bda1a6c312096
Stefan Plösser <stefan@nvon.com>
2022-03-19 12:15:06 +0100

    use react toolbox and icon on default page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,31 @@
-import { Button, Paper } from "@mui/material";
+import { Box, Button, Paper } from "@mui/material";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import { allZero } from "@nvon/react-toolbox";
 import type { NextPage } from "next";
 import * as React from "react";
+import { RocketStart } from "../src/generated/svg-components/RocketStart";
 
 const Home: NextPage = () => {
   return (
     <Container maxWidth="lg">
       <Paper sx={{ p: 2, mt: 2, mb: 2 }}>
         <Typography variant="h1">Hello World</Typography>
-        <Typography variant="h2">Now start creating</Typography>
+        <Typography variant="h2">Now start creating </Typography>
       </Paper>
       <Button sx={{ mr: 2 }}>Primary</Button>
       <Button color="secondary">Secondary</Button>
+      <Box
+        sx={{
+          position: "fixed",
+          opacity: 0.02,
+          textAlign: "center",
+          ...allZero,
+          "& svg": { height: "100%" },
+        }}
+      >
+        <RocketStart />
+      </Box>
     </Container>
   );
 };
